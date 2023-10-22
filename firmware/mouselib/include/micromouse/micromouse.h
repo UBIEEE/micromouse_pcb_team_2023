e6b92b5cba4f0e5cc756a4fe6a1a55033651b546
Peter Lilley <hi.petelilley@gmail.com>
2023-10-22 14:58:44 -0400

    Created initial CMake project

---
+++ firmware/mouselib/include/micromouse/micromouse.h
@@ -0,0 +1,6 @@
+#ifndef __MICROMOUSE_H__
+#define __MICROMOUSE_H__
+
+
+
+#endif // __MICROMOUSE_H__
---
+++ firmware/programs/main/src/main.c
@@ -0,0 +1,8 @@
+#include <micromouse/micromouse.h>
+
+#include <pico/stdlib.h>
+#include <stdio.h>
+
+int main() {
+  stdio_init_all();
+}
