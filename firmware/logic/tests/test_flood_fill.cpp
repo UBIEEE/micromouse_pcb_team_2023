f06925367d44d71784f6d9e14c4992a75636a9a2
Peter Lilley <hi.petelilley@gmail.com>
2024-02-18 13:04:01 -0500

    Added GoogleTest

---
+++ firmware/logic/tests/test_flood_fill.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <mouse_logic/maze.hpp>
+#include <mouse_logic/solver.hpp>
+
+TEST(FloodFill, Test1) {
+  Maze maze;
+  FloodFillSolver solver(maze);
+}
+
+TEST(FloodFill, Test2) {
+  Maze maze;
+  FloodFillSolver solver(maze);
+}
+
